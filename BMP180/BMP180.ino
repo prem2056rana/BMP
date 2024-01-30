0190f0290371975dc18b7754a9ddd54f64f692f5
prem2056rana <prem2056rana@gmail.com>
2024-01-30 10:20:02 +0545

    v0.1

---
+++ BMP180/BMP180.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP085_U.h>
+
+Adafruit_BMP085_Unified bmp;
+
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  }
+}
+
+void loop() {
+  sensors_event_t event;
+  bmp.getEvent(&event);
+  
+  float temperature;
+  bmp.getTemperature(&temperature);
+  
+  float pressure;
+  bmp.getPressure(&pressure);
+  
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  
+  Serial.print("Pressure: ");
+  Serial.print(pressure);
+  Serial.println(" Pa");
+  
+  delay(1000);
+}
